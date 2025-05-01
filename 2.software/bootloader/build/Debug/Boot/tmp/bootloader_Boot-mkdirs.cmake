c856ff70f236c09f1b95012375fbcdd4e45a3bff
stranding <45762837+StrandingNeko@users.noreply.github.com>
2025-05-01 09:58:50 +0800

    feat: init bootloader project.

---
+++ 2.software/bootloader/build/Debug/Boot/tmp/bootloader_Boot-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/Boot"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/Boot/build"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/tmp"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/src/bootloader_Boot-stamp"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/src"
+  "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/src/bootloader_Boot-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/src/bootloader_Boot-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "E:/2.Git/github/stm32h7r7_study/2.software/bootloader/build/Debug/Boot/src/bootloader_Boot-stamp${cfgdir}") # cfgdir has leading slash
+endif()
